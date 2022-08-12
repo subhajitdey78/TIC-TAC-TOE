61576bb16ce47133d3c6f0de16031916fafed990
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2022-08-12 11:48:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-import "./App.css";
-import TicTacToe from "./components/TicTacToe";
+import React, { useContext } from 'react';
+import {ThemeContext} from './ThemeProvider';
+import './App.css'
 
 function App() {
+  const {toggle, toggleFunction} = useContext(ThemeContext)
+ 
   return (
-    <div>
-      <TicTacToe />
+    <div className='app' style={{backgroundColor: toggle ? 'pink' : 'white'}} >
+       <h1> Context API</h1>
+       <button onClick={toggleFunction} > Change the theme</button>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
