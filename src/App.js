82ed414cb848d738e483520c55c957ad767f4efa
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2022-08-12 21:35:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, { useContext } from 'react';
-import {ThemeContext} from './ThemeProvider';
-import './App.css'
+import React from 'react';
+import './App.css';
+import { ThemeContext } from './theme-context'
 
 function App() {
-  const {toggle, toggleFunction} = useContext(ThemeContext)
- 
-  return (
-    <div className='app' style={{backgroundColor: toggle ? 'pink' : 'white'}} >
-       <h1> Context API</h1>
-       <button onClick={toggleFunction} > Change the theme</button>
-    </div>
 
-  )
+  
+  const { theme, toggle, dark } = React.useContext(ThemeContext)
+  
+
+  return (
+    <div className='app'>
+         <h1>Theme Toggler</h1>
+         <div   
+            className='app_header'
+            onClick={toggle}
+              style={{
+                backgroundColor: theme.backgroundColor,
+                color: theme.color
+              }}
+         
+         >
+           <h1>Context API Theme Toggler</h1>
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tincidunt tincidunt tincidunt. Proin condimentum placerat laoreet. Suspendisse ex urna, vehicula sagittis imperdiet nec, rhoncus ac urna. Morbi pretium dictum nisi, vitae dapibus purus ultrices sed. Mauris tellus nunc, luctus eu orci nec, aliquam bibendum nulla. Nam ullamcorper pharetra turpis semper placerat. In vulputate luctus mauris, pellentesque rutrum mauris sagittis et. Nunc efficitur orci id velit ullamcorper efficitur. Mauris vitae dolor convallis, molestie velit quis, facilisis mauris. Suspendisse venenatis quam quam, vestibulum bibendum magna venenatis sit amet. Vestibulum nec urna ullamcorper, ornare ex eu, auctor nisl. Nunc faucibus ut erat in euismod. Quisque in ornare ligula.
+
+              Nulla feugiat vitae est in fermentum. Nam leo velit, vulputate id massa ac, condimentum elementum dolor. Integer at nisi pretium, varius diam nec, auctor ligula. Nam semper dui feugiat risus facilisis blandit. Aliquam quis lorem eu velit volutpat aliquam eget nec enim. Integer condimentum luctus accumsan. Vivamus non tortor elit. Proin dapibus massa a mi lobortis dictum. Nunc in egestas nulla.</p>
+           <div 
+              
+              className="button_container"
+            >
+              {`Toggle to ${!dark  ? 'Dark' : 'Light'} theme`}
+           </div>
+         </div>
+         
+            
+     
+    </div>
+  );
 }
 
-export default App
+export default App;
